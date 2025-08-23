5389f90765ffdab373a6c6946b394f32c7e75137
zotel1 <zotelsigel@gmail.com>
2025-08-22 23:17:34 -0300

    En este ejercicio utilizamos input para que el usuario interactue con el codigo a traves de la consola

---
+++ ejercicio_tres.py
@@ -0,0 +1,8 @@
+#3- Escribir un programa que pregunte el nombre al usuario y que luego muestre por pantalla un saludo y el contenido de la variable ingresada por el usuario.
+
+def presentacion(nombre):
+    return (nombre)
+
+nombre = str(input('Ingrese su nombre: '))
+
+print(f'Bienvenido {nombre} ')
