9a89074a18f489031097fcb8c49f6fa1f2673ebb
zotel1 <zotelsigel@gmail.com>
2025-09-16 17:36:08 -0300

    ejemplo terminado, quite los parametros de entrada a la funcion, y accedi a la funcion como si fuera una tupla

--- 12-09/piedra_papel_tijera.py
+++ 12-09/piedra_papel_tijera.py
@@ -2,74 +2,61 @@
 import random
 lista =["piedra", "papel", "tijera"]
 #usuario = "s"
-contador_h_total = 0
-contador_r_total = 0
+contador_humano_total = 0
+contador_robot_total = 0
 #humano = ""
 piedra = "piedra"
 papel = "papel"
 tijera = "tijera"
-contador_humano = 0
-contador_robot = 0
-contador_humano_suma = 0
-contador_robot_suma = 0
 
 
 usuario = str(input("¿Queres jugar?: s/n "))
 while (usuario == "s"):
     humano = str(input("¿Que elegis? ¿Piedra? ¿Papel? o ¿Tijera?: "))
     robot = random.choice(lista)
-    contador_humano = 0
-    contador_robot = 0
-    print(f"El Robot eligio {robot}")
     
-    puntos = 0
-    def quien_gana(puntos):
+    print(f"El Robot eligio {robot}")
+
+    
+    
+    
+    def quien_gana():
+        contador_humano = 0
+        contador_robot = 0
         
-    #def quien_gana(contador_humano, contador_robot):
-        if(humano != ""):
+    # def quien_gana(contador_humano, contador_robot):
+        if(humano != ""):    
             if(humano == robot):
                 print("¡Empate!")
             elif (humano == piedra and robot == papel):
-                contador_humano += 1
+                contador_humano = contador_humano + 1
                 print("¡Gana humano!")
             elif(humano == papel and robot == piedra):
-                contador_humano += 1
+                contador_humano = contador_humano + 1
                 print("¡Gana humano!")
             elif( humano == tijera and robot == papel):
-                contador_humano += 1
+                contador_humano = contador_humano + 1
                 print("¡Gana humano!")
-            #elif (robot == piedra and humano == papel):
-            #    contador_robot += 1
-             #   print(f"¡Gana el Robot y suma {contador_robot} punto!")
-            #elif(robot == papel and humano == piedra):
-             #   contador_robot += 1
-              #  print(f"¡Gana el Robot y suma {contador_robot} punto!")
-            #elif(robot == tijera and humano == papel):
-             #   contador_robot += 1
-              #  print(f"¡Gana el Robot y suma {contador_robot} punto!")
             else:
-                contador_robot += 1
+                contador_robot = contador_robot + 1
                 print("¡Gana Robot!")
-            puntos += (contador_robot or contador_humano)
         else:
             print("Por favor elija uno")
 
-            
-        
-        return puntos
-    contador_humano += contador_humano_suma
-    contador_robot += contador_robot
-    print(f"¡WAUU! Suma {quien_gana(puntos)} punto más!")
-        
-    #print("Aca anted sel elif")
-  #  contador_h_total += quien_gana(contador_humano , contador_robot)
-   # contador_r_total += quien_gana(contador_robot, contador_humano)
+        return (contador_humano, contador_robot)
+    
+    #h = quien_gana[0]
+    #r = quien_gana[1]
+    h, r = quien_gana()
+    
+    contador_humano_total += h
+    contador_robot_total += r
+    
+    print(f"¡WAUU! El humano suma {contador_humano_total} mientras el Robot lleva {contador_robot_total} punto más!")
+    
     usuario = str(input("¿Queres jugar?: s/n "))
     if (usuario == "n"):
-        #print("Durante")
-        print(f"Juego terminado usted hizo {contador_h_total} puntos y el robot hizo: {contador_r_total} puntos")
+        print(f"Juego terminado usted hizo {contador_humano_total} puntos y el robot hizo: {contador_robot_total} puntos")
         
     else:
         print(f"¡Fin del juego!")
-    
-#print("Fin del juego")
