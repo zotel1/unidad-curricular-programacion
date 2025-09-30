3681c7c0aeb50655c123db82c94fc48940f1b52f
zotel1 <zotelsigel@gmail.com>
2025-09-30 14:49:12 -0300

    version de prueba, no me funciona la entrada manual de edades

--- 26-9/ejercicio01.py
+++ 26-9/ejercicio01.py
@@ -1,23 +1,41 @@
 menu = ("Menu del cine")
 print(menu)
-
+edad_usuario_total = 0
 
 entrada_cine = str(input("Quiere entrar a ver una pelicula?: [s/n]"))
+
 while(entrada_cine == "s"):
-    def control_acceso()->str:
-        edad_espectador = int(input("Ingrese su edad: "))
+    edad_entero = int(input("Ingrese su edad: "))
+    if (edad_entero == ""):
+        print("¡Por favor ingresa un numero entero!")
+    else:
+        
+        edad_usuario_total += edad_entero
+        print(f"Edad prueba {edad_usuario_total}")
+        break
+    
+          
+
+#entrada_cine = str(input("Quiere entrar a ver una pelicula?: [s/n]"))
+if(entrada_cine == "s"):
+    while(entrada_cine == "s"):
+        edad += edad_usuario
+        edad = (input("Ingrese su edad: "))
         entrada_vip = str(input("Tiene entrada vip? [s/n]: "))
-        if(edad_espectador != int('inf')):
-            if(edad_espectador < 12):
-                print("Acceso restringido.")
-            elif(edad_espectador > 12 and edad_espectador < 17):
-                print("Puede entrar solo con acompañante.")
-            elif(edad_espectador > 17 and entrada_vip == "s"):
-                print("Acceso total.")
-            elif(edad_espectador > 17 and entrada_vip == "n"):
-                    print("Acceso general.")
-        return f"Para el usuario de {edad_espectador} años"
-entrada_cine = str(input("Quiere entrar a ver una pelicula?: [s/n]"))
+        if(edad < 12):
+            print("Acceso restringido.")
+        elif(edad > 12 and edad < 17):
+            print("Puede entrar solo con acompañante.")
+        elif(edad > 17 and entrada_vip == "s"):
+            print("Acceso total.")
+        elif(edad > 17 and entrada_vip == "n"):
+            print("Acceso general.")
+        #else:
+        #    print("Por favor ingrese un numero entero.")
+        
+    entrada_cine = str(input("Quiere entrar a ver una pelicula?: [s/n]"))
+else:
+    print("Solo se acepta el ingreso de las teclas: [s/n]")
 
 
 
